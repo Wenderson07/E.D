17d6ccc7fb8d7de314d54babdff07c92984eed8e
Wenderson07 <Wenderson_pac@hotmail.com>
2017-09-21 09:57:11 -0300

    Update fdssf.vhd

--- fdssf.vhd
+++ fdssf.vhd
@@ -1,18 +1,34 @@
--- importa std_logic da IEEE library
-library IEEE;
-use IEEE.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
--- Declara uma entidade
-entity ANDGATE is
-   port ( 
-         IN1 : in std_logic;
-         IN2 : in std_logic;
-         OUT1: out std_logic);
-end ANDGATE;
-architecture RTL of ANDGATE is
+	
+entity atividade2 is
+	port
+	(
+		-- Input ports
+		X0	: in  std_logic;
+		X1 : in  std_logic;
+		X2 : in  std_logic;
+		X3 : in  std_logic;
+		-- Output ports
+		M  : out std_logic
+		
+		
+end atividade2;
+
+
+
+architecture behavior of atividade2 is
+
+	signal S0: std_logic;
+	signal S1: std_logic;
+	signal S2: std_logic;
 
 begin
 
-  OUT1 ⇐ IN1 and IN2;
+	S0 <= C nand C;
+	S1 <= A nand S0;
+	S2 <= B nand C;
+	S <= S1 nand S2;
 
-end RTL;
+end behavior;
